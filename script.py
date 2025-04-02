12c97ba1a1e666807eba2ea8e7b6640d45c64d7f
Михаил <michlea@yandex.ru>
2025-04-02 15:39:22 +0300

    feat: add all themes at matan

--- script.py
+++ script.py
@@ -11,22 +11,27 @@ def file_has_h1(file_path):
                 return True
     return False
 
-
 def update_headings(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
     
     updated_lines = []
+    modified = False  # Track changes
+
     for line in lines:
         match = re.match(r"^(#{1,5}) (.*)", line)
         if match:
             updated_heading = f"#{match[1]} {match[2]}\n"  # Ensure newline is preserved
+            if updated_heading != line:  # Check if change is needed
+                modified = True
             updated_lines.append(updated_heading)
         else:
-            updated_lines.append(line)  # Non-heading lines remain unchanged
+            updated_lines.append(line)
 
-    with open(file_path, "w", encoding="utf-8") as file:
-        file.writelines(updated_lines)
+    if modified:  # Only write if changes were made
+        print(file_name)
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.writelines(updated_lines)
 
 for root, _, files in os.walk(vault_path):
     for file in files:
